eaf89022a9cbde2eca913e967c8c70d53f2e9904
= <=>
2015-07-02 18:35:24 -0700

    Fixed carousel

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -15,7 +15,7 @@
             <div class="item {{ ($counter == 1) ? 'active' : '' }}">
                 <div class="row">
                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                        <img alt="{{ $c->title }}" class="img-responsive img-center" src="/images/shows/{{ $c->season }}/{{ $c->show_image }}" />
+                        <img alt="{{ $c->title }}" class="img-responsive img-center" src="/images/shows/{{ getCurrentSeason() }}/{{ $c->show_image }}" />
 
                     </div>
                     <div class="col-md-6 col-sm-6 col-xs-12">
