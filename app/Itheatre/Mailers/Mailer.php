20ada6b5badb99303c29c9b2d534db59ca68e27e
Scott Hummel <scotthummel@icloud.com>
2016-02-25 19:08:13 -0700

    Changed over to Mailgun

--- app/Itheatre/Mailers/Mailer.php
+++ app/Itheatre/Mailers/Mailer.php
@@ -6,7 +6,7 @@ abstract class Mailer {
 
     public function sendToItc($view, $data, $subject) {
         return \Mail::send($view, $data, function($message) use($subject) {
-           $message->to('itheatrecollaborative@cox.net')
+           $message->to(env('ITHEATRE_EMAIL'))
                ->subject($subject);
         });
     }
